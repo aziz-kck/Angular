9e50ad9f8ca02a03caf8b65dc23fc2e53c2b184a
aziz-kck <61433292+aziz-kck@users.noreply.github.com>
2024-06-21 16:39:50 +0100

    2

--- src/app/product/product.component.spec.ts
+++ src/app/product/product.component.spec.ts
@@ -1,57 +1,87 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import { ActivatedRoute } from '@angular/router';
+// import { ComponentFixture, TestBed } from '@angular/core/testing';
+// import { HttpClientModule } from '@angular/common/http';
+//
+// import { ProductComponent } from './product.component';
+//
+// describe('ProductComponent', () => {
+//   let component: ProductComponent;
+//   let fixture: ComponentFixture<ProductComponent>;
+//
+//   beforeEach(() => {
+//     TestBed.configureTestingModule({
+//       declarations: [ProductComponent]
+//     });
+//     fixture = TestBed.createComponent(ProductComponent);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
+//   });
+//
+//   it('should create', () => {
+//     expect(component).toBeTruthy();
+//   });
+// });
+
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { ProductComponent } from './product.component';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { ProductService } from '../services/product.service';
 import { StockService } from '../services/stock.service';
-import { HttpClientModule } from '@angular/common/http'; // Import HttpClientModule
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
 
 describe('ProductComponent', () => {
   let component: ProductComponent;
   let fixture: ComponentFixture<ProductComponent>;
-  let productService: jasmine.SpyObj<ProductService>;
-  let stockService: jasmine.SpyObj<StockService>;
+  let productService: ProductService;
+  let stockService: StockService;
 
-  beforeEach(async () => {
-    productService = jasmine.createSpyObj('ProductService', ['fetchData', 'addProduct', 'deleteData']);
-    stockService = jasmine.createSpyObj('StockService', ['fetchData']);
+  // Mock ActivatedRoute with a mockSnapshot method
+  const mockActivatedRoute = {
+    snapshot: { paramMap: { get: () => '1' } }
+  };
 
-    await TestBed.configureTestingModule({
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
       declarations: [ProductComponent],
-      imports: [
-        HttpClientModule // Include HttpClientModule
-      ],
       providers: [
         NgbModal,
-        {
-          provide: ProductService,
-          useValue: productService
-        },
-        {
-          provide: StockService,
-          useValue: stockService
-        },
-        {
-          provide: ActivatedRoute,
-          useValue: {
-            snapshot: {
-              paramMap: {
-                get: () => '1' // Mocking route snapshot parameter 'id'
-              }
-            }
-          }
-        }
+        ProductService,
+        StockService,
+        { provide: ActivatedRoute, useValue: mockActivatedRoute }
       ]
     }).compileComponents();
-  });
+  }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ProductComponent);
     component = fixture.componentInstance;
+    productService = TestBed.inject(ProductService);
+    stockService = TestBed.inject(StockService);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should fetch data successfully', () => {
+    const mockResponse = { /* Mock your response data here */ };
+    spyOn(productService, 'fetchData').and.returnValue(of(mockResponse));
+
+    component.ngOnInit(); // Trigger ngOnInit manually
+
+    expect(component.data).toEqual(mockResponse);
+  });
+
+  it('should handle null id in fetchData method', () => {
+    spyOn(mockActivatedRoute.snapshot.paramMap, 'get').and.returnValue(null);
+    spyOn(console, 'error'); // Spy on console.error
+
+    component.fetchData();
+
+    expect(console.error).toHaveBeenCalledWith('No ID found in route snapshot.');
+    expect(productService.fetchData).not.toHaveBeenCalled(); // Ensure fetchData is not called
+  });
+
+  // Add more tests as needed for other methods and behaviors
 });
