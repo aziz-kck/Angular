7d6e261caa6f10b5bec1acf1dc1675a346a6829f
aziz-kck <61433292+aziz-kck@users.noreply.github.com>
2024-06-21 16:35:15 +0100

    Update product.component.spec.ts

--- src/app/product/product.component.spec.ts
+++ src/app/product/product.component.spec.ts
@@ -1,15 +1,51 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { ActivatedRoute } from '@angular/router';
 import { ProductComponent } from './product.component';
+import { ProductService } from '../services/product.service';
+import { StockService } from '../services/stock.service';
+import { HttpClientModule } from '@angular/common/http'; // Import HttpClientModule
 
 describe('ProductComponent', () => {
   let component: ProductComponent;
   let fixture: ComponentFixture<ProductComponent>;
+  let productService: jasmine.SpyObj<ProductService>;
+  let stockService: jasmine.SpyObj<StockService>;
+
+  beforeEach(async () => {
+    productService = jasmine.createSpyObj('ProductService', ['fetchData', 'addProduct', 'deleteData']);
+    stockService = jasmine.createSpyObj('StockService', ['fetchData']);
+
+    await TestBed.configureTestingModule({
+      declarations: [ProductComponent],
+      imports: [
+        HttpClientModule // Include HttpClientModule
+      ],
+      providers: [
+        NgbModal,
+        {
+          provide: ProductService,
+          useValue: productService
+        },
+        {
+          provide: StockService,
+          useValue: stockService
+        },
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            snapshot: {
+              paramMap: {
+                get: () => '1' // Mocking route snapshot parameter 'id'
+              }
+            }
+          }
+        }
+      ]
+    }).compileComponents();
+  });
 
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [ProductComponent]
-    });
     fixture = TestBed.createComponent(ProductComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
